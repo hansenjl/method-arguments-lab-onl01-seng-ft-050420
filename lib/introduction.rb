e912e101376365c1035ef1ffc9300863b15bdad8
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2020-05-04 19:29:25 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,8 +1,8 @@
 #introduction takes in an argument of a name and puts out a phrase with that name using string interpolation
 
 
-def introduction(name)
-  puts "Hi, my name is #{name}."
+def introduction(x)
+  puts "Hi, my name is #{x}."
 end
 
 
